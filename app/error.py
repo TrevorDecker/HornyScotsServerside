98708174160b6b8cbfdcfbc531b33f82a6665bd6
Vaibhav Gupta <vaibhavtheory@gmail.com>
2014-07-11 21:34:15 -0700

    server boiler plate

--- app/error.py
+++ app/error.py
@@ -1,3 +1,4 @@
 error = {
-  "NOT_LOGGED_IN": {"code": -2, "message":"Authentication error"}
+  "NOT_LOGGED_IN": {"code": -2, "message":"Authentication error"},
+  "SUCCESS": {"code": -1, "message":"Action completed!"},
 }
--- app/server.py
+++ app/server.py
@@ -1,18 +1,24 @@
 from functools import wraps
-from flask import Flask, jsonify, g
+from flask import Flask, jsonify, session
 from error import error
 import grubhub
 
 app = Flask(__name__)
 
+# helper functions
 def login_required(f):
   @wraps(f)
   def decorated_function(*args, **kwargs):
-    if not hasattr(g, 'user'):
-      return jsonify(error["NOT_LOGGED_IN"])
-    return f(*args, **kwargs)
+    if 'token' in session:
+      if session['token']:
+        return f(*args, **kwargs)
+    return jsonify(error["NOT_LOGGED_IN"])
   return decorated_function
 
+def get_param(param):
+  return request.form[param]
+
+# routes
 @app.route("/")
 def hello():
   return "This is not accessble to the public"
@@ -26,29 +32,53 @@ def signup(email=None, password=None, first_name=None, last_name=None):
   return gh.do_signup(email, password, first_name, last_name)
 
 @app.route("/login", methods=["POST"])
-@login_required
-def login(email=None, password=None):
-  return gh.do_login(email, password)
+def login():
+  email = get_param('email')
+  password = get_param('pass')
+
+  user = gh.do_login(email, password)
+  if user is not None:
+    session['token'] = result.token
+  return jsonify(error["NOT_LOGGED_IN"])
 
 @app.route("/logout", methods=["POST"])
 @login_required
 def logout():
-  return
+  del session['token']
+  return jsonify(error["SUCCESS"])
 
-@app.route("/search", methods=["POST"])
+@app.route("/search/<count>", methods=["POST"])
 @login_required
-def search():
-  return
+def search(count=None):
+  count = int(count)
+  lat = float(get_param("lat"))
+  lng = float(get_param("lng"))
+
+  # spawn async search on grubhub db
+  # return session id
+
+  return jsonify(error["SUCCESS"])
 
 @app.route("/recall", methods=["POST"])
 @login_required
 def recall():
-  return
+  session_id = get_param('session')
+
+  # query db and return
+
+  return jsonify({})
 
 @app.route("/order", methods=["POST"])
 @login_required
 def order():
-  return
+  session_id = get_param('session')
+  meal_id = get_param('id')
+  address = get_param('address')
+  payment = get_param('pay')
+
+  # create order
+
+  return jsonify({})
 
 if __name__ == "__main__":
   app.run(port=5001)
