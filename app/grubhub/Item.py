0780dbbd4f9f8fa9d57202eaff3ec7661bb791a1
Vaibhav Gupta <vaibhavtheory@gmail.com>
2014-07-12 08:13:22 -0700

    server workings

--- app/grubhub/Item.py
+++ app/grubhub/Item.py
@@ -26,6 +26,15 @@ class Item:
             self.options = None
         self.config = {}
 
+    def to_json(self):
+      return {
+        "id": self.id,
+        "name": self.name,
+        "config": self.config,
+        "type": self.type,
+        "price": self.price,
+      }
+
     def get_tag(self, tags):
         if tags:
             if "DRINK" in tags["tag"]:
@@ -51,7 +60,7 @@ class Item:
     def add_option(self, id, desc, data):
         if id not in self.config:
             self.config[id] = {"desc": desc, "select": []}
-        self.config[id]["select"].append(data)
+        self.config[id]["select"].append((data["@id"], data["description"]))
     
     def set_option(self, option_id, options, cash):
         insert = 0
@@ -61,7 +70,7 @@ class Item:
         for option in options_list:
             price = float(option.get("option-price", 0))
             price_ratio = price/cash
-            if price_ratio > random() or (insert < options["min"] and random() < 0.4):
+            if price_ratio > random() or (insert < options["min"] and random() > 0.3):
                 self.price += price
                 self.add_option(option_id, options["desc"], option)
                 insert += 1
--- app/grubhub/parllel.py
+++ app/grubhub/parllel.py
@@ -2,45 +2,51 @@
 import thread,time
 import threading
 import random
+from Menu import Meal
+
 import sys
 sys.path.append('../db')
 import db
-import grubhub as gh
+TAX = 1.0875
+TIP = 1.15
+TOTAL_SPEND = 15
 
 #must be defined before startSearch
 #serach id is the session id for th serach
 #tryNum is the number that is being used this is mostly just a seed
-def thread_func(search_id,(tryNum,(lat,lng,rest_ids))):
-    meal = None
-    tryCount = 0
-    maxTrys = 10
-    DB = db.DB()
-    rest_id = rest_ids(random.randint(0,len(rest_ids)))
-    menu = gh.rest_menu(rest_id,lat,lng)
-    while meal  is None and tryCount < maxTrys:
-        meal = Meal()
-        meal = meal.generate_meal(menu)
-    if order is None:
-        #send fail message
-        DB.fail_thread(search_id)
-    else:
-         #send sucss message to server 
-         DB.add_meal(meal,search_id)
+def thread_func(search_id,(tryNum,(gh,lat,lng,rest_list))):
+  meal = None
+  rest = None
+  DB = db.DB()
+  while meal is None:
+    rest_index = random.randint(0,len(rest_list) - 1)
+    rest = rest_list.pop(rest_index)
+    menu = gh.rest_menu(rest["id"],lat,lng)
+    meal = Meal(total = (TOTAL_SPEND/TAX - rest["deliveryFee"]) / TIP)
+    meal, price = meal.generate_meal(menu)
+    if price < rest["orderMin"]:
+      meal = None
+  if meal is None:
+      #send fail message
+    DB.fail_thread(search_id)
+  else:
+    #send sucss message to server
+    order = meal[0] + meal[0.5] + meal[1] + meal[2] + meal[3]
+    print order
+    content = { "rest": rest, "order": order}
+    DB.add_meal(content,search_id)
 
 
 #called by controler 
-def startSearch(neededMeals,serach_id,lat,lan,restids):
-    maxTrys = 2*neededMeals
-    tryNum = 0;
-    meals = 0;
-    while (tryNum <= maxTrys and meals <= neededMeals): 
-        tryNum += 1
-        try:
-            thread.start_new_thread(thread_func,(serach_id,(tryNum,(lat,lan,restids))))
-            success += 1
-        except:
-           print "thread failed trying again\n"
-    #wait until all the threads are done
-    while(threading.active_count() > 1):
-        time.sleep(1)
-    return  meals >= neededMeals
+def start_search(gh, neededMeals,serach_id,lat,lan,rest_list):
+  maxTrys = 2*neededMeals
+  tryNum = 0
+  success = 0
+  while (tryNum <= maxTrys and success <= neededMeals): 
+    tryNum += 1
+    try:
+      thread.start_new_thread(thread_func,(serach_id,(tryNum,(gh,lat,lan,rest_list))))
+      success += 1
+    except:
+      print "thread failed trying again\n"
+  return success
